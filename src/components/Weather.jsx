71243b839f4256ed18e0cfbedd89d9853f8dbb11
Suguru Miyata <suguru1114@gmail.com>
2024-12-03 18:45:52 +1100

    Initial commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Weather.css'
+
+function Weather() {
+  return (    
+    <div className='weather'>
+        <div className='serch-bar'>
+            <input type="text" placeholder='Search'></input>
+        </div>
+    </div>
+  )
+}
+
+export default Weather
