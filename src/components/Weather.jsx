1380023c07d631ee004bbffea1abece45ec46bd8
Suguru Miyata <suguru1114@gmail.com>
2024-12-12 20:32:04 +1100

    get data from api

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Weather.css'
 import search_icon from '..//assets/search.png'
 import rain_icon from '..//assets/rain.png'
@@ -11,13 +11,42 @@ import windy_icon from '..//assets/windy.png'
 
 function Weather() {
 
-    const search = async()=>{
+
+    const [wearherData, setWeatherData] = useState(false);
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "04n": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d":rain_icon,
+        "10n":rain_icon,
+        "13d":snow_icon,
+        "13n":snow_icon,
+        
+    }
+
+    const search = async(city)=>{
         try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
         
             const response = await fetch(url);
             const data = await response.json();
             console.log(data);
+            const icon = alllIcons[data.weather[0].icon] || clear_icon;
+
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            })
 
         } catch (error) {   
             console.error("Error fetching weather data:", error);
